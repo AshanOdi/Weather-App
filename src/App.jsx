40e4e325d547c1efd44325470ef3ca9f84551953
AshanOdi <ashanuni25@gmail.com>
2025-09-20 23:45:22 +0530

    Routes created 👍

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="text-3xl text-red-50">Hi</div>;
+  return;
+  <BrowserRouter>
+    <div>
+      <Toaster position="top-right" />
+      <Routes path="/*">
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  </BrowserRouter>;
 }
 
 export default App;
