49eaaa307c08004cc0d91d9901de0eb1e57d2d04
AshanOdi <ashanuni25@gmail.com>
2025-09-21 04:02:15 +0530

    login page ui refined 🤒

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -4,13 +4,19 @@ export default function LoginPage() {
   const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 
   return (
-    <div>
+    <div className="w-full bg-yellow-500 min-h-screen bg-fixed   bg-cover  bg-[url('/back.jpg')]  flex flex-col justify-center items-center">
       {!isAuthenticated ? (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-red-600"
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </button>
       ) : (
         <div>
           <h2>Welcome, {user.name}</h2>
           <button
+            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
             onClick={() =>
               logout({ logoutParams: { returnTo: window.location.origin } })
             }
