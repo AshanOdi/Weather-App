0216557fcd6b1246fdb6b4c604c99a7a39f26e85
AshanOdi <ashanuni25@gmail.com>
2025-09-21 01:53:13 +0530

    login page routes created 📢

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/loginPage";
+import HomePage from "./pages/homePage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
-
   return (
-    <div>
-      {!isAuthenticated ? (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
-      ) : (
-        <div>
-          <h2>Welcome, {user.name}</h2>
-          <button
-            onClick={() =>
-              logout({ logoutParams: { returnTo: window.location.origin } })
-            }
-          >
-            Log Out
-          </button>
-        </div>
-      )}
-    </div>
+    <BrowserRouter>
+      <div>
+        <Toaster position="top-right" />
+        <Routes path="/*">
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
